f0cb229ef4b49ed3b83bd473c785e7adcf1d0cf1
Victorvick399 <victorvick399@gmail.com>
2019-11-05 11:56:29 +0300

    initialize gitty app

--- src/app/gitty/gitty.component.ts
+++ src/app/gitty/gitty.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import {GittyHttpService} from '../gitty-http/gitty-http.service';
+import{User} from '../user-details/user'
+import{HttpClient} from '@angular/common/http';
+import{Repos} from '../repos/repos';
 
 @Component({
-  selector: 'app-gitty',
+  selector: 'app-search',
   templateUrl: './gitty.component.html',
   styleUrls: ['./gitty.component.css']
 })
+
 export class GittyComponent implements OnInit {
-
-  constructor() { }
-
+  
+      profile: any = {
+        avatar_url: ''
+      };
+      repos: any = [];
+      username: string;
+      user: any;;
+    
+    
+    
+      constructor(private httpService:GittyHttpService) {
+        this.httpService.getUser().subscribe(user => {
+          this.user = user;
+          this.username = "";
+        });
+        this.httpService.getUser().subscribe(profile => {
+          this.profile = profile
+        })
+        this.httpService.getRepos().subscribe(repos => {
+          this.repos = repos;
+        });
+    
+      }
+    
+      bioInfo() {
+        this.httpService.getData(this.username);
+        this.httpService.userRequest()
+        this.httpService.getData(this.username);
+        this.httpService.getUser().subscribe(profile => {
+          this.httpService.userRequest()
+          this.user = this.httpService.user
+          this.profile = profile
+        });
+        this.httpService.getUser().subscribe(repos => {
+          this.repos = repos;
+        });
+      }
+    
   ngOnInit() {
+    
   }
 
+  
 }
