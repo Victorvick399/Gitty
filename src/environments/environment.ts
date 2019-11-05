b9e3aee4f3324acb82313c70a212fd7076af2f0e
Victorvick399 <victorvick399@gmail.com>
2019-11-05 11:52:02 +0300

    hide API key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false ,
+  // giToken:'499d61d1686ebed9b3256b298f2aee03ed935f79',
+
+  APIURL: "https://github.com/settings/tokens/",
+  
+  APIKEY:"?access_token=499d61d1686ebed9b3256b298f2aee03ed935f79"
 };
 
 /*
