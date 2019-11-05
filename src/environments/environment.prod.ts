8fe51df5d3147eb2d9398ccd7a6fbd45cb487ea8
Victorvick399 <victorvick399@gmail.com>
2019-11-05 11:52:16 +0300

    hide API key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  APIURL: "https://github.com/settings/tokens/",
+  
+  APIKEY:"?access_token=499d61d1686ebed9b3256b298f2aee03ed935f79"
 };
