1af33a3f71d7de3c8b657d8a6fb24bef6a3d81eb
Victorvick399 <victorvick399@gmail.com>
2019-11-05 11:54:11 +0300

    routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GittyComponent } from './gitty/gitty.component';
+import { GittySearchComponent } from './gitty-search/gitty-search.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+import { GittyAboutComponent } from './gitty-about/gitty-about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'gitty', component: GittyComponent},
+  { path: 'search', component: GittySearchComponent},
+  {path: 'about' , component: GittyAboutComponent},
+  { path:'', redirectTo:"/about", pathMatch:"full"},
+  { path:'**', component:ErrorPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
